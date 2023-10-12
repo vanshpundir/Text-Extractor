79a67bac44aaf1d9df196012ef84b21e7ea2a5ee
vansh pundir <vansh@Vanshs-MacBook-Air-3.local>
2023-10-12 10:15:26 +0530

    Made some changes in rotate_image.py

--- script/rotate_image.py
+++ script/rotate_image.py
@@ -1,40 +1,25 @@
 import cv2
-import numpy as np
-import math
-# Readim image
-img = cv2.imread("/Users/vansh/PycharmProjects/Text-Extractor/images/extract_data/image_text.png")
+import pytesseract
 
-# Convert to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Read the image
+img = cv2.imread("/Users/vansh/PycharmProjects/Text-Extractor/images/extract_data/test.png")
 
-# Find edges using Canny edge detection
-edges = cv2.Canny(gray, 50, 200)
+# Use pytesseract to extract orientation
+text = pytesseract.image_to_osd(img)
+lines = text.splitlines()
 
-# Run Hough transform on edges to find lines
-lines = cv2.HoughLinesP(edges, 1, np.pi/180, threshold=100, minLineLength=100, maxLineGap=5)
-
-# Extract angles and lengths of lines
-angles = []
+# Find the line that contains orientation information
 for line in lines:
-    x1, y1, x2, y2 = line[0]
-    angle = math.atan2(y2 - y1, x2 - x1)
-    length = np.sqrt((y2 - y1)**2 + (x2 - x1)**2)
-    angles.append(angle)
-
-# Find average angle
-angle_sum = sum(angles)
-num_angles = len(angles)
-avg_angle = angle_sum / num_angles
-
-# Calculate rotation angle needed to make image horizontal
-rotation_angle = -np.rad2deg(avg_angle)
+    if "Orientation in degrees" in line:
+        rotation_angle = float(line.split(":")[1].strip())
+        break
 
 # Get center and dimensions
 height, width = img.shape[:2]
-center = (width/2, height/2)
+center = (width / 2, height / 2)
 
 # Rotate image
-rot_mat = cv2.getRotationMatrix2D(center, rotation_angle, 1)
+rot_mat = cv2.getRotationMatrix2D(center, -rotation_angle, 1)
 rotated = cv2.warpAffine(img, rot_mat, (width, height))
 
 # Save rotated image
