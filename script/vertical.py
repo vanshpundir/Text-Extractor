3c9ca2f76ca6caf66df264b9e868e8a65227b441
vansh pundir <vansh@Vanshs-MacBook-Air-3.local>
2023-10-12 10:43:54 +0530

    Modified to Class vertical.py; If the image is vertical it will convert it into horizontal for the image extraction;

--- script/vertical.py
+++ script/vertical.py
@@ -1,37 +1,44 @@
 from PIL import Image
 import pytesseract
-# Read the image using Pillow
-img = Image.open("/Users/vansh/PycharmProjects/Text-Extractor/images/extract_data/test.png")
 
-# Check the orientation using Tesseract
-text = pytesseract.image_to_osd(img)
-lines = text.splitlines()
+class VerticalToHorizontal:
+    def __init__(self, image_path):
+        self.image = Image.open(image_path)
+        self.orientation = self._check_orientation()
 
-# Variable to track if a horizontal heading is found
-horizontal_heading_found = False
+    def _check_orientation(self):
+        text = pytesseract.image_to_osd(self.image)
+        lines = text.splitlines()
+        for line in lines:
+            if "Orientation in degrees" in line:
+                detected_orientation = float(line.split(":")[1].strip())
+                return detected_orientation
+        return 0
 
-# Iterate through the detected lines
-for line in lines:
-    if "Orientation in degrees" in line:
-        detected_orientation = float(line.split(":")[1].strip())
-    elif "Text: " in line:
-        # Check if the text is horizontal
-        if "Horizontal" in line:
-            horizontal_heading_found = True
+    def rotate(self):
+        if self.orientation == 0:
+            return self.image  # Image is already horizontal
 
-# If a horizontal heading is found, the image is likely already horizontal
-if horizontal_heading_found:
-    rotated_img = img
-else:
-    # Rotate the image by 90 degrees
-    if detected_orientation > 0:
-        rotated_img = img.rotate(90, expand=True)
-    else:
-        rotated_img = img.rotate(-90, expand=True)
+        if self.orientation > 0:
+            rotated_img = self.image.rotate(90, expand=True)
+        else:
+            rotated_img = self.image.rotate(-90, expand=True)
 
-# Ensure the image is in RGB mode (not RGBA) before saving as JPEG
-if rotated_img.mode == 'RGBA':
-    rotated_img = rotated_img.convert('RGB')
+        if rotated_img.mode == 'RGBA':
+            rotated_img = rotated_img.convert('RGB')
 
-# Save the rotated image as JPEG
-rotated_img.save('rotated.jpg', 'JPEG')
+        return rotated_img
+
+    def save_as_jpeg(self, output_path):
+        rotated_img = self.rotate()
+        rotated_img.save(output_path, 'JPEG')
+
+    def save_as_png(self, output_path):
+        rotated_img = self.rotate()
+        rotated_img.save(output_path, 'PNG')
+
+# Example usage:
+if __name__ == '__main__':
+    image_path = "/Users/vansh/PycharmProjects/Text-Extractor/images/extract_data/test.png"
+    converter = VerticalToHorizontal(image_path)
+    converter.save_as_jpeg('rotated.jpg')
