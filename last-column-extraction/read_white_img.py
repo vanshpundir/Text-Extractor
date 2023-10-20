8711de0247523cd31bc07e586cfcd19ea3835710
vanshpundir <vanshpundir44@gmail.com>
2023-10-20 14:58:23 +0530

    added columl preprcoessingp

---
+++ last-column-extraction/read_white_img.py
@@ -0,0 +1,25 @@
+import cv2
+img = cv2.imread("/Users/cup72/PycharmProjects/Text-Extractor/last-column-extraction/white_image.png")
+width, height , _  = img.shape
+print(width, height)
+for i in range(width):
+    for j in range(height):
+        if img[:,:,0][i,j]==0:
+            img[:,:,0][i,j]=255
+            img[:,:,1][i, j] = 255
+            img[:,:,2][i, j] = 255
+#cv2.imshow("New Image",img)
+for i in range(width):
+    for j in range(100):
+        if (img[:,:,0][i,j] and img[:,:,1][i,j] and img[:,:,2][i,j]) <80 :
+            intensity=img[:,:,0][i,j]
+            break
+for i in range(width):
+    for j in range(height):
+        if img[:,:,0][i,j]==intensity:
+            img[:,:,0][i,j]=255
+            img[:,:,1][i, j] = 255
+            img[:,:,2][i, j] = 255
+cv2.imshow("New Image",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
