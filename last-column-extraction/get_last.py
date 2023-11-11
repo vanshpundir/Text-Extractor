c415d5d8217a52548f9b58f32978e141ed9f541c
vansh <vanshpundir44@gmail.com>
2023-11-11 19:59:32 +0530

    added get_last.py; best till yet for extracting last column

--- last-column-extraction/get_last.py
+++ last-column-extraction/get_last.py
@@ -5,8 +5,22 @@ with open("/Users/vansh/PycharmProjects/Text-Extractor/output/rotated/res_0.txt"
         data = json.loads(line)
         break
 
-print(data['res'])
-last_column_positions = [entry['text_region'][-1][0] for entry in data['res']]
-max_last_column_position = max(last_column_positions)
+# Assuming data['res'] contains a list of entries, each having a list of points in 'text_region'
+all_boxes = [point for entry in data['res'] for point in entry['text_region']]
 
+# Sort the bounding boxes based on X-coordinate
+sorted_boxes = sorted(all_boxes, key=lambda box: box[0])
 
+# Select the boxes with the highest X-coordinates as the last column
+last_column_boxes = sorted_boxes[-4:]  # Assuming each entry has 4 points
+
+# Calculate the bounding box for the last column
+x_min = min(point[0] for point in last_column_boxes)
+x_max = max(point[0] for point in last_column_boxes)
+y_min = min(point[1] for point in last_column_boxes)
+y_max = max(point[1] for point in last_column_boxes)
+
+print(f"X-Min: {x_min}")
+print(f"X-Max: {x_max}")
+print(f"Y-Min: {y_min}")
+print(f"Y-Max: {y_max}")
---
+++ last-column-extraction/get_last_2.py
@@ -0,0 +1,30 @@
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+# Image path
+image_path = "/Users/vansh/PycharmProjects/Text-Extractor/output/rotated/[1, 37, 2904, 857]_0.jpg"
+
+# Bounding box coordinates
+bbox = [[2741.0, 0.0], [2756.0, 479.0], [2756.0, 809.0], [2741.0, 809.0]]
+
+# Read the image
+image = cv2.imread(image_path)
+
+# Display the image
+fig, ax = plt.subplots(1)
+ax.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+# Add width only to the left side of the bounding box
+width = 220  # Adjust the width as needed
+bbox[0][0] -= width
+bbox[3][0] -= width
+
+# Create a Rectangle patch
+rect = patches.Rectangle((bbox[0][0], bbox[0][1]), bbox[1][0] - bbox[0][0], bbox[2][1] - bbox[0][1], linewidth=2, edgecolor='r', facecolor='none')
+
+# Add the patch to the Axes
+ax.add_patch(rect)
+
+# Show the image with the annotated bounding box
+plt.show()
