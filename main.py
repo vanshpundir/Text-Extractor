1371f21db74034622dcd765420d967d556dda142
vansh <vanshpundir44@gmail.com>
2023-12-02 17:11:20 +0530

    added changes in the main.py

--- main.py
+++ main.py
@@ -6,19 +6,24 @@ import pandas as pd
 from model.trocr_inference import TrOCRInference
 import re
 import cv2
+import shutil
+from model.inference import Inference
+from models.extract_text import extractText
 
 
 class Main:
-    def __init__(self, file_path, text_path, image_path, image_dir):
+    def __init__(self, file_path, text_path, image_path, image_dir, model_path):
         self.file_path = file_path
         self.image_dir = image_dir
         self.image_path = image_path
         self.ocr_inference = TrOCRInference()
         self.last_column = LastColumn(text_path, image_path)
+        self.mnist_model = model_path
+        self.model = Inference(self.mnist_model)
 
     def get_last_column(self):
         bbox, cropped_image = self.last_column.extract_last_column()
-        save_image = "cropped_image.jpg"
+        save_image = "/Users/vansh/PycharmProjects/Text-Extractor/cropped_image.jpg"
         cv2.imwrite(save_image, cropped_image)
 
         return cropped_image, save_image
@@ -36,7 +41,13 @@ class Main:
         processor.apply_horizontal_section_extractor(
             "image_processing_text/result_denoised.jpg",
             'image_processing_text/processed_image/horizontal_image')
- 
+        processor.apply_digit_processor('image_processing_text/processed_image/horizontal_image',
+                                        'image_processing_text/processed_image/single_digit')
+
+    def mnist_result(self, single_digit='image_processing_text/processed_image/single_digit'):
+        self.model.mnist_result(single_digit)
+
+
     def extract_last_column(self):
         # Read the Excel file
         df = pd.read_excel(self.file_path)
@@ -46,13 +57,28 @@ class Main:
 
         return last_column
 
+    @staticmethod
+    def numerical_sort(value):
+        parts = value.split('_')
+        try:
+            return int(parts[1].split('.')[0])  # Extract numeric part and remove file extension
+        except (IndexError, ValueError):
+            # Handle cases where the expected pattern is not found or conversion to int fails
+            return float('inf')
+
     def get_two_digit_number(self):
         last_column = self.extract_last_column()
         numbers = []
+        image_files = []
+        for i in os.listdir(image_dir):
+            if i.endswith(".png") or i.endswith(".jpg"):
+                image_files.append(i)
+        image_files = sorted(image_files, key=self.numerical_sort)
+        print(image_files)
 
         # Get a list of image files ending with .png, sorted
-        image_files = sorted([f for f in os.listdir(self.image_dir) if f.endswith(".png")])
 
+        print("IMage file array is:", image_files)
         for filename in image_files[1:]:  # Skip the first image
             image_path = os.path.join(self.image_dir, filename)
             print(image_path)
@@ -74,7 +100,7 @@ class Main:
                 continue
 
             digit_text = self.ocr_inference.perform_ocr(image_path)
-            print("digit text is: ",digit_text)
+            print("digit text is: ", digit_text)
             digits = re.findall(r'\d+', digit_text)
 
             if len("".join(digits)) == 2:
@@ -131,15 +157,18 @@ class Main:
 
         print("Deletion process completed.")
 
+
 if __name__ == "__main__":
     # Replace the file path, image_dir, and model_path with your specific paths
+    full_image = '/Users/vansh/PycharmProjects/Text-Extractor/output/rotated/[1, 37, 2904, 857]_0.jpg'
+    extractText(full_image)
     file_path = "script/output/rotated/[2, 2, 2902, 846]_0.xlsx"
     image_dir = "image_processing_text/processed_image/horizontal_image"
     model_path = "model/mnist_model_final.h5"
     text_path = "output/rotated/res_0.txt"
-    image_path = "output/rotated/[1, 37, 2904, 857]_0.jpg"
+    image_path = "/Users/vansh/PycharmProjects/Text-Extractor/output/rotated/[1, 37, 2904, 857]_0.jpg"
     # Create an instance of the Main class and call the get_two_digit_number method
-    main_instance = Main(file_path, text_path, image_path=image_path, image_dir=image_dir)
+    main_instance = Main(file_path, text_path, image_path=image_path, image_dir=image_dir, model_path=model_path)
     main_instance.horizontal_image()
     main_instance.delete_small_height_images(20)
-
+    main_instance.mnist_model("single_digit_dir")
