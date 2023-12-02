4b74f3a29c2dd8365036bf5e76166ca368d07e16
vansh <vanshpundir44@gmail.com>
2023-12-02 15:12:04 +0530

    updated extract_text.py;

--- models/extract_text.py
+++ models/extract_text.py
@@ -1,9 +1,19 @@
 import subprocess
-
+import os
 
 def extractText(image_path):
-    # Define the command as a list of strings
+    # Get the directory of the current script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
 
+    # Define the relative path to the Text-Extractor directory
+    project_dir = os.path.dirname(script_dir)
+    # Combine the script directory with the relative path to get the full path
+
+    # Change the current working directory to Text-Extractor
+
+    os.chdir(project_dir)
+
+    # Define the command as a list of strings
     command = ["paddleocr", f"--image_dir={image_path}", "--type=structure", "--layout=True"]
 
     # Run the command
@@ -11,5 +21,7 @@ def extractText(image_path):
         subprocess.run(command, check=True, shell=False)
     except subprocess.CalledProcessError as e:
         print("An error occurred:", e)
-
-
+#
+if __name__ == "__main__":
+    image_path = 'image_processing/rotated.jpg'  # Using a relative path for the image
+    extractText(image_path)
